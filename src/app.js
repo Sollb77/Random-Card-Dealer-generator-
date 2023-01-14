897174e3b7ac1300149e25c68085f040f74ad82d
Sollb77 <qsiecuadorservicios@gmail.com>
2023-01-14 16:12:46 +0000

    final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardList = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  const Simbolo = ["♥", "♣", "♠", "♦"];
+  let EligeNumero = Math.floor(Math.random() * cardList.length);
+  let EligeSimbolo = Math.floor(Math.random() * Simbolo.length);
+
+  let Heart1 = document.getElementById("heart1");
+  let Heart2 = document.getElementById("heart2");
+  let Numero = document.getElementById("numero");
+
+  Heart1.innerHTML = Simbolo[EligeSimbolo];
+
+  Heart2.innerHTML = Simbolo[EligeSimbolo];
+  Numero.innerHTML = cardList[EligeNumero];
+  if (Heart1.innerText === "♥" || Heart1.innerText === "♦") {
+    Heart1.style.color = "red";
+  }
+  if (Heart2.innerText === "♥" || Heart2.innerText === "♦") {
+    Heart2.style.color = "red";
+  }
 };
